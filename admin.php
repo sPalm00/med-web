fa37693eac7777efa0882229c7431042601a4394
sPalm00 <s58123425024@ssru.ac.th>
2019-03-07 14:04:31 +0700

    Test commit

--- admin.php
+++ admin.php
@@ -289,7 +289,7 @@
     </div>
     <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="250"> 
         <div class="container-fluid">
-			<div class="navbar-header">
+			<div class="navbar-header"> 
 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
