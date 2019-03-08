328da3cf124417d40b4c95e196cf80c6a4cf0e17
sPalm00 <s58123425024@ssru.ac.th>
2019-03-08 16:32:34 +0700

    Edit web

--- logout.php
+++ logout.php
@@ -1,5 +1,16 @@
 <?php
 session_start();
-session_destroy(;
-header("lovation: form.php");
 ?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+session_unset(); 
+
+session_destroy();
+  header('location: login.php');
+?>
+
+</body>
+</html>
